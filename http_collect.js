6694599f4813e3a06fc9ad0c35fb044b3c90e2ba
Marcelo Ramos <marcelor@gmail.com>
2015-05-05 01:26:38 -0300

    Solution to Http Collect exercise

---
+++ http_collect.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var bl = require('bl');
+
+var url = process.argv[2];
+
+function displayData(response) {
+    response.pipe(bl(function (err, data) {
+        if (err)
+            return console.error(err)
+
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }));
+}
+
+http.get(url, displayData);
